af8a94b0af38f7c1a8ea0a163e74dbed3387f1e8
text-code <dssn2102@gmail.com>
2022-10-23 11:11:10 +0900

    first commit

---
+++ app/src/main/java/com/textcode/github/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.textcode.github
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.textcode.github.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.topAppBar.setNavigationOnClickListener {
+            binding.drawerLayout.open()
+        }
+
+        binding.navigationView.setNavigationItemSelectedListener { menuItem ->
+            // Handle menu item selected
+            menuItem.isChecked = true
+            binding.drawerLayout.close()
+            true
+        }
+    }
+}
